769c0dc75ea9e7fcfdff6ed677e901b0af8a5545
andyChenHuaYing <461857202@qq.com>
2015-01-06 22:06:57 +0800

    Finish Web Design with JavaScript and the Document Object Model's 4 chapter --A JavaScript Image Gallery

---
+++ items-javascript/js_image/js/showPicture.js
@@ -0,0 +1,12 @@
+function switchPicture (whichPicture) {
+	var source = whichPicture.href;
+	var placeholder = document.getElementById('placeholder');
+	placeholder.setAttribute('src', source);
+	document.getElementById('description').firstChild.nodeValue = whichPicture.getAttribute('title');
+}
+
+function countBodyChildren () {
+	alert(document.getElementsByTagName('body')[0].childNodes.length);		
+}	
+
+window.onload = countBodyChildren;
