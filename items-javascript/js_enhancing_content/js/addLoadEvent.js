7d8dccfa66570e66e68549e3188d40174b115d0e
andychen <461857202@qq.com>
2015-01-09 09:51:43 +0800

    Reconfiguration JavaScript chapter 8.

---
+++ items-javascript/js_enhancing_content/js/addLoadEvent.js
@@ -0,0 +1,20 @@
+/**
+ * Created by andy on 1/7/2015.
+ */
+
+/**
+ * Multiple execute window.onload;
+ */
+function addEvent(fun){
+	var oldFunction = window.onload;
+	if (!oldFunction) {
+		window.onload = fun;
+	} else {
+		window.onload = function () {
+			oldFunction();
+			fun();
+		}
+	}
+}
+
+addEvent(displayAbbreviations);
