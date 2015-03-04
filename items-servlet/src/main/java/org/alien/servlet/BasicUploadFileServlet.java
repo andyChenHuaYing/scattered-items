87eddf06f5ff99095378ed2ab75f2b1a63217b67
andychen <461857202@qq.com>
2015-03-04 16:47:12 +0800

    Add items-servlet model , remember build Apache Tomcat lib dependency and scattered-items model dependency.

---
+++ items-servlet/src/main/java/org/alien/servlet/BasicUploadFileServlet.java
@@ -0,0 +1,26 @@
+package org.alien.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Happy daily, happy life.<br>
+ * =========================<br>
+ * Description:<br>
+ * Created by andychen on 2015/3/4.<br>
+ * Version 1.0-SNAPSHOT<br>
+ */
+public class BasicUploadFileServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ items-servlet/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/css/showPicture.css"/>
+<script src="${pageContext.request.contextPath}/js/jquery-2.1.3.js"></script>
+<body>
+<div id="showPicture">
+    <h2>File Upload</h2>
+    <img id="preview" class="preview" src="imgs/img-2-benchi.jpg" alt="benchi"/><br/><br/>
+    <a href="imgs/img-2-benchi.jpg"><img src="imgs/img-1-benchi.jpg" alt="benchi"/></a>
+    <a href="imgs/img-2-chenhong.jpg"><img src="imgs/img-1-chenhong.jpg" alt="chenhong"/></a>
+    <a href="imgs/img-2-liuyan.jpg"><img src="imgs/img-1-liuyan.jpg" alt="liuyan"/></a>
+    <a href="imgs/img-2-liuyifei.jpg"><img src="imgs/img-1-liuyifei.jpg" alt="liuyifei"/></a>
+    <a href="imgs/img-2-shishi.jpg"><img src="imgs/img-1-shishi.jpg" alt="shishi"/></a>
+</div>
+</body>
+</html>
